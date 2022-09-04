3a21b711395d85cd66faf000b5029ede0488c45d
Anshu Rajpoot <107042248+AnshuRajpoot25@users.noreply.github.com>
2022-09-04 11:38:24 +0530

    Add files via upload

---
+++ Append a List.py.py
@@ -0,0 +1,16 @@
+list=[]
+size=int(input("enter the size of list:"))
+for i in range(1,size+1):
+    a=int(input("enter the elements of list"))
+    list.append(a)
+even=[]
+odd=[]
+for j in list:
+    if j%2==0:
+        even.append(j)
+        print("list of even",even)
+    else:
+        odd.append(j)
+        print("list of odd",odd)
+        
+    
